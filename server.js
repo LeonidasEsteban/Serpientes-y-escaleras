8b56de649a01a43b53926ac2805deeea66305e36
Leonidas Esteban <leonidasesteban@gmail.com>
2013-08-18 15:12:36 -0500

    hola

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express();
 
 app.get('/',function (req, res){
-	res.send('Te quiero Pau!!');
+    res.send('Te quiero Pau!!');
 });
-
+app.get("/mensaje/:mensaje", function (req, res){
+    res.send(req.params.mensaje);
+});
 app.listen(3000);
