64b46fb8debd3189283610c4dfbbbb394642bcb6
Leonidas Esteban <leonidasesteban@gmail.com>
2013-08-19 23:25:45 -0500

    multiplayer beta

--- public/juego.js
+++ public/juego.js
@@ -3,12 +3,26 @@ $(function(){
     var posicion = 0;
     var cols, rows, totalgrid;
     var $target;
+    var playerID;
 
     window.client = io.connect(window.location.href);
 
     client.on('avanzar',function(data){
-        juego.avanzando(0,data.posicion);
+        console.log(data);
+        juego.avanzando(0,data.posicion,data.id);
     });
+    client.on('newPlayer',function(data){
+        juego.newPlayer(data.id);
+        console.log(data.total);
+        console.log("el data mover es "+data.mover);
+        if(data.mover !== undefined){
+            playerID = data.mover;
+        }
+        crearJugadores();
+    });
+    // window.client.emit('newPlayer',{
+    //     id : playerIDlast
+    // });
     window.juego = {
         grid : function(){
             rows = 8;
@@ -27,9 +41,9 @@ $(function(){
                 var random = Math.floor((Math.random()*5));
                 var tipo;
                 if(random == 1){
-                    tipo = 'escalera';
+                    tipo = '';
                 }else if(random == 2 ){
-                    tipo = 'serpiente';
+                    tipo = '';
                 }else{
                     tipo = "";
                 }
@@ -39,35 +53,42 @@ $(function(){
         avanzar : function(){
             $('.avanzar').on('click',function(){
                 var random  = Math.floor((Math.random()*6)+1);
+                var movimiento = playerID;
                 posicion    = posicion + random;
-                juego.avanzando(0, posicion);
+                juego.avanzando(0, posicion,playerID);
+                juego.realtime(posicion,movimiento);
+
             });
         },
-        realtime : function(posicion){
+        realtime : function(posicion,jugadorID){
             window.client.emit('avanzar',{
                 posicion : posicion,
+                id : jugadorID
             });
         },
-        avanzando : function(tiempo, posicion){
+        avanzando : function(tiempo, posicion, jugador){
 
             var ir = setTimeout(function(){
-                $("body").scrollTop($('.jugador').position().top - 100);
+                // $("body").scrollTop($('.jugador').position().top - 100);
 
-                if(posicion > totalgrid ){
-                    posicion = (totalgrid) - (posicion - (totalgrid));
-                }else if(posicion == totalgrid){
-                    console.log('Ganaste!');
-                    win();
-                }
+                // if(posicion > totalgrid ){
+                //     posicion = (totalgrid) - (posicion - totalgrid);
+                // }else if(posicion == totalgrid){
+                //     console.log('Ganaste!');
+                //     win();
+                // }
 
                 $target = $('.col[data-count='+posicion+']');
-                $target.addClass('active');
+                // $target.addClass('active');
+                if($target.data('count') !== ""){
+                    var left    = $target.position().left;
+                    var top     = $target.position().top;
+                    $('#player'+jugador).css({'left':left+30,'top':top+30});
+                    juego.validacion();
+                }
 
-                var left    = $target.position().left;
-                var top     = $target.position().top;
-                $('.jugador').css({'left':left+30,'top':top+30});
-                juego.validacion();
             },tiempo);
+
 
             function win(){
                 $('.win').addClass('active').fadeOut().fadeIn();
@@ -75,29 +96,37 @@ $(function(){
         },
         validacion :function(){
             var delay = setTimeout(function(){
-                if($target.hasClass('serpiente')){
-                    posicion = posicion - 4;
-                    console.log('serpiente');
-                    $target.removeClass('active');
-                    juego.avanzando(300,posicion);
-                    $target.removeClass('active');
-                }else if ($target.hasClass('escalera')){
-                    posicion = posicion + 5;
-                    console.log('escalera');
-                    juego.avanzando(300,posicion);
-                    $target.removeClass('active');
-                }
-            juego.realtime(posicion);
-                
+                // if($target.hasClass('serpiente')){
+                //     posicion = posicion - 4;
+                //     console.log('serpiente');
+                //     $target.removeClass('active');
+                //     juego.avanzando(300,posicion,playerID);
+                //     $target.removeClass('active');
+
+                // }else if ($target.hasClass('escalera')){
+                //     posicion = posicion + 5;
+                //     console.log('escalera');
+                //     juego.avanzando(300,posicion,playerID);
+                //     $target.removeClass('active');
+
+                // }
+
+
+
 
             },1000);
+
+        },
+        newPlayer : function(id){
+            $('#grid').append('<div class="jugador" id="player'+id+'"></div>');
         },
         init : function(){
             juego.grid();
             juego.avanzar();
+            // juego.newPlayer(0);
             $('.col:first').removeClass('escalera serpiente');
             $('.col:last').removeClass('escalera serpiente');
-            $("body").scrollTop($('.jugador').position().top);
+            // $("body").scrollTop($('.jugador').position().top);
         }
     };
     juego.init();
--- server.js
+++ server.js
@@ -25,11 +25,21 @@ app.get('/',function (req, res){
 app.get("/mensaje/:mensaje", function (req, res){
     res.send(req.params.mensaje);
 });
-
+var id= 0;
+var lista = [];
 var connection = function(socket){
     console.log('Hola');
+    var movimientos = [];
     socket.on('avanzar',function(data){
         socket.broadcast.emit('avanzar',data);
     });
+    id++;
+    lista.push(id);
+    for(i = 1; i <= lista.length; i++){
+        socket.emit('newPlayer',{id: i, mover:i , total:lista});
+    }
+    var ultimo = lista.length - 1;
+    socket.broadcast.emit('newPlayer',{id: lista[ultimo]});
+
 };
 io.sockets.on('connection', connection);
